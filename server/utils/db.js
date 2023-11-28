9b2556bc9568cefa6c1c15e3f142cc50264b1ea9
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2023-11-28 23:47:02 +0530

    created a simple user Schema model

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose")
-const mongoUrl="mongodb://localhost:27017/students"
+const mongoUrl="mongodb://127.0.0.1:27017/mern_practice"
 
 
 const connectDb= async ()=> {
     try {
         await mongoose.connect(mongoUrl)
         console.log("connection to DB was  as success")
+        console.log("-------------",mongoose.connection.readyState,"--------------------------")
+
     } catch (error) {
         console.log("connection to DB was  failed",error)
         
